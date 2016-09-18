c5e7e25dd12c99ac61071f59b66e3b6d07958ba0
Pavel Djundik <github@xpaw.me>
2016-09-18 10:09:33 +0300

    Add tests

---
+++ ValvePak/ValvePak.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Valve Pak Library - Unit Tests")]
+[assembly: AssemblyDescription("Unit tests for library to work with Valve Pak archives")]
+[assembly: AssemblyCompany("SteamDatabase")]
+[assembly: AssemblyProduct("ValvePak")]
+[assembly: AssemblyCopyright("Copyright © SteamDatabase 2016")]
+
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9112a44-85ff-43a8-bb73-c9cf583cc5f8")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
